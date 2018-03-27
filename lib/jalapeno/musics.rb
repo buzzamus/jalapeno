ee44c4f7ccb7ee86098abb1b83af144de6c98132
Brent Busby <brent.busby@yahoo.com>
2018-03-27 05:45:34 -0500

    turn all tabs to spaces to avoid extra spaces

--- lib/jalapeno/musics.rb
+++ lib/jalapeno/musics.rb
@@ -1,9 +1,9 @@
 module Musics
   quotes = ["I hurt myself today to see if I still feel. I focus on the pain. The only thing that's real",
-  					"If there's a bustle in your hedgerow, Don't be alarmed now, It's just a spring clean for the May Queen",
-  				  "Nothing's gonna change my world", "The world is a game to be played", "Time takes a cigarette, puts it in your mouth",
-  					"Don't critize what you can't understand", "I feel like i'm looking for my soul, like a poor man looking for gold",
-  					"I know that I was born and I know that i'll die. The in between is mine", "War, children, it's just a shot away"]
+            "If there's a bustle in your hedgerow, Don't be alarmed now, It's just a spring clean for the May Queen",
+            "Nothing's gonna change my world", "The world is a game to be played", "Time takes a cigarette, puts it in your mouth",
+            "Don't critize what you can't understand", "I feel like i'm looking for my soul, like a poor man looking for gold",
+            "I know that I was born and I know that i'll die. The in between is mine", "War, children, it's just a shot away"]
     QUOTE =  quotes[rand(0..quotes.length - 1)]
 
 end
--- test/test_jalapeno.rb
+++ test/test_jalapeno.rb
@@ -2,32 +2,32 @@ require 'test/unit'
 require 'jalapeno'
 
 class TestJalapeno < Test::Unit::TestCase
-	def test_version_string
-		assert_equal Jalapeno.version_string, "Jalapeno version #{Jalapeno::VERSION}"
-	end
+  def test_version_string
+    assert_equal Jalapeno.version_string, "Jalapeno version #{Jalapeno::VERSION}"
+  end
 
-	def test_movie_quote
-		assert_equal Jalapeno.movie, "#{Movies::QUOTE}"
-	end
+  def test_movie_quote
+    assert_equal Jalapeno.movie, "#{Movies::QUOTE}"
+  end
 
-	def test_book_quote
-		assert_equal Jalapeno.book, "#{Books::QUOTE}"
-	end
+  def test_book_quote
+    assert_equal Jalapeno.book, "#{Books::QUOTE}"
+  end
 
-	test "original quote displayed should match module original quote" do
-		assert_equal Jalapeno.original, "#{Originals::QUOTE}"
-	end
+  test "original quote displayed should match module original quote" do
+    assert_equal Jalapeno.original, "#{Originals::QUOTE}"
+  end
 
-	test "Music quote displayed should match class music quote" do
-		assert_equal Jalapeno.music, "#{Musics::QUOTE}"
-	end
+  test "Music quote displayed should match class music quote" do
+    assert_equal Jalapeno.music, "#{Musics::QUOTE}"
+  end
 
-	test "Facts displayed should match class facts listed" do
-		assert_equal Jalapeno.fact, "#{Facts::TRIVIA}"
-	end
+  test "Facts displayed should match class facts listed" do
+    assert_equal Jalapeno.fact, "#{Facts::TRIVIA}"
+  end
 
-	test "Fake trivia displayed should match class facts fake trivia listed" do
-		assert_equal Jalapeno.fake_fact, "#{Facts::FAKE}"
-	end
+  test "Fake trivia displayed should match class facts fake trivia listed" do
+    assert_equal Jalapeno.fake_fact, "#{Facts::FAKE}"
+  end
 
 end
